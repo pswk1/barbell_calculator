73c09b16f8cb18779da313b0a6887273a223595d
Peter Kang <peeterkang@gmail.com>
2022-01-21 20:59:58 -0800

    add footer link

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider, Link, Box, theme } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 import Banner from './components/Banner';
 import Barbell from './components/Barbell';
+
+const year = new Date().getFullYear();
 
 export const App = () => (
 	<ChakraProvider theme={theme}>
 		<Banner />
-    <Barbell />
+		<Barbell />
+		<Box marginY={8} textAlign={'center'}>
+			Developed by {' '}
+			<Link href='https://www.linkedin.com/in/peterswkang/' isExternal>
+				 Peter Kang <ExternalLinkIcon mx='2px' />
+			</Link>
+			{' '} {year}
+		</Box>
 	</ChakraProvider>
 );
